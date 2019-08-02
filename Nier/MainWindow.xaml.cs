af34920148cc7d174c16b89aa061601cc5b9754e
thunshell <30592752+thunshell@users.noreply.github.com>
2019-08-02 17:18:21 +0800

    Add files via upload

---
+++ Nier/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nier
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Loaded += MainWindow_Loaded;
+        }
+
+
+        private DrawingBrush _gridBrush;
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_gridBrush == null)
+            {
+                _gridBrush = new DrawingBrush(new GeometryDrawing(
+                    new SolidColorBrush(Colors.White),
+                         new Pen(new SolidColorBrush(Colors.LightGray), 1.0),
+                             new RectangleGeometry(new Rect(0, 0, 10, 10))));
+                _gridBrush.Stretch = Stretch.None;
+                _gridBrush.TileMode = TileMode.Tile;
+                _gridBrush.Viewport = new Rect(0.0, 0.0, 10, 10);
+                _gridBrush.ViewportUnits = BrushMappingMode.Absolute;
+                //grid.Background = _gridBrush;
+            }
+        }
+    }
+}
